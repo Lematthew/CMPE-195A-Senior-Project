eeb2fbe41569943aab6fb54beb14a97206d123ab
Lematthew <74629769+Lematthew@users.noreply.github.com>
2022-03-01 19:05:48 -0800

    StorePage Params

--- grocer-brochure/src/Components/StorePage.js
+++ grocer-brochure/src/Components/StorePage.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
-const StorePage = () => (
+const StorePage = () =>{ 
+  
+  const id = useParams();
+  
+  
+  return (
   <main style={{ padding: "1rem 0" }}>
-  <h2>This is a Store Page!</h2>
-</main>
-);
-
+  <h2>This is a Store Page! </h2>
+  </main>
+  );
+}
 export default StorePage;
--- grocer-brochure/src/index.js
+++ grocer-brochure/src/index.js
@@ -22,7 +22,7 @@ ReactDOM.render(
       <Route path="/" element={<App />} />
       <Route path="Login" element={<Login />} />
       <Route path="About" element={<About />} />
-      <Route path="StorePage" element={<StorePage/>} />
+      <Route path="StorePage/:id" element={<StorePage/>} />
       <Route path="Signup" element={<SignUp/>} />
       <Route path="Cart" element={<Cart/>} />
     </Routes>
