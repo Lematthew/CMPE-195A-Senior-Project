db7573ef356fb3821774c60b160f1193120579d8
Vishnu Adda <addavishnu@gmail.com>
2022-03-03 05:18:56 -0800

    Login CSS

--- grocer-brochure/src/Navbar.js
+++ grocer-brochure/src/Navbar.js
@@ -23,6 +23,11 @@ function Navbar(){
               <p>About</p>
             </div>
           </a>
+          <a href="/Login" id="login-tag">
+            <div class="svg-container">
+              <p>Log in</p>
+            </div>
+          </a>
           <a href="/Signup" id="signup-tag">
             <div class="svg-container">
               <p>Sign Up</p>
@@ -35,33 +40,6 @@ function Navbar(){
           </a>
         </div>
       </div>
-
-//     <div>
-//              <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
-//   <ReactBootStrap.Container>
-//   <ReactBootStrap.Navbar.Brand href="/">GrocerBrochure</ReactBootStrap.Navbar.Brand>
-//   <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//   <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-//     <ReactBootStrap.Nav className="me-auto">
-//       <ReactBootStrap.Nav.Link href="/Login">Log in</ReactBootStrap.Nav.Link>
-//       <ReactBootStrap.Nav.Link href="#pricing">Pricing</ReactBootStrap.Nav.Link>
-//       <ReactBootStrap.NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-//         <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
-//         <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
-//         <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
-//         <ReactBootStrap.NavDropdown.Divider />
-//         <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
-//       </ReactBootStrap.NavDropdown>
-//     </ReactBootStrap.Nav>
-//     <ReactBootStrap.Nav>
-//       <ReactBootStrap.Nav.Link href="/About">About</ReactBootStrap.Nav.Link>
-//       <ReactBootStrap.Nav.Link href="/SignUp"> Sign Up</ReactBootStrap.Nav.Link>
-//     </ReactBootStrap.Nav>
-//   </ReactBootStrap.Navbar.Collapse>
-//   </ReactBootStrap.Container>
-// </ReactBootStrap.Navbar>
-
-//     </div>
     );
 }
 export default Navbar;
