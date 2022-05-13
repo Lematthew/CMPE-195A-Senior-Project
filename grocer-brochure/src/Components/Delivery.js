25f10a0ff6c797ec63189d7e83daa1c92c1e886c
Vishnu Adda <addavishnu@gmail.com>
2022-05-13 13:36:08 -0700

    Quick UI fix to cards. (#9)

--- grocer-brochure/src/Components/Delivery.js
+++ grocer-brochure/src/Components/Delivery.js
@@ -5,10 +5,7 @@ import "./styles/Delivery.css";
 const Delivery = () => {
     return (
         <main>
-            <div className='directions-box'>
-                <p className='direction-miles'>0.7 miles</p>
-                <p>East Santa Clara Street</p>
-            </div>
+            
         </main>
     )
 };
